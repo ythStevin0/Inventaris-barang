0b918810a5b4bcf543351d859f7708765dedcd31
ythStevin0 <404Stevino@gmail.com>
2026-06-07 00:06:34 +0700

    chore: remove dummy inventory seeders and fix deletion

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            InventorySeeder::class,
-            InventoryTransactionSeeder::class,
         ]);
     }
 }
